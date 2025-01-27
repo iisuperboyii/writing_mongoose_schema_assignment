1c99b4a23d6a2082eb29ed12c93bbc5948296c45
iiuperboyii <aaronjomon24@gmail.com>
2025-01-27 15:22:17 +0530

    added schema.js and added necessary information

---
+++ schema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  roles: {
+    type: [String],
+    default: ['user']
+  },
+  profile: profileSchema,
+  lastLogin: {
+    type: Date
+  }
+});
+
+// Export the User model
+module.exports = mongoose.model('User', userSchema);
